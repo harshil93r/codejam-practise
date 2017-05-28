269438720e57bcb7dc3cd62abe6c8c1ea4503a26
Harshil <harshil.rastogi@innovaccer.com>
2017-05-28 15:42:42 -0400

    print removed from reverse_words

--- reverse_words.py
+++ reverse_words.py
@@ -6,5 +6,4 @@ total_cases = int(f.readline())
 for case in range(total_cases):
     words = f.readline().strip().split()
     words.reverse()
-    print words
     o.write('Case #%d: ' % (case + 1) + ' '.join(words) + '\n')
